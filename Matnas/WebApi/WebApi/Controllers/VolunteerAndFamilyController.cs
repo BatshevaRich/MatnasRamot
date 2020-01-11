45502e5f9e480ea8d2c206f5a1872e749f665b87
Batsheva Rich <batsheva7424@gmail.com>
2020-01-11 20:24:15 +0200

    added volunteerAndFamily controller with functions

---
+++ Matnas/WebApi/WebApi/Controllers/VolunteerAndFamilyController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Common;
+namespace WebApi.Controllers
+{
+    public class VolunteerAndFamilyController : ApiController
+    {
+        // GET: api/VolunteerAndFamily
+        public IEnumerable<VolunteerAndFamily> Get()
+        {
+            return Bll.VolunteerAndFamilyManager.GetVolunteerAndFamilys();
+        }
+
+        // GET: api/VolunteerAndFamily/5
+        public VolunteerAndFamily Get(int id)
+        {
+            return Bll.VolunteerAndFamilyManager.GetVolunteerAndFamily(id);
+        }
+
+        // POST: api/VolunteerAndFamily
+        public void Post(VolunteerAndFamily newvolunteerAndFamily)
+        {
+            Bll.VolunteerAndFamilyManager.AddVolunteerAndFamily(newvolunteerAndFamily);
+        }
+
+        // PUT: api/VolunteerAndFamily/5
+        public void Put(int id, [FromBody]VolunteerAndFamily value)
+        {
+            Bll.VolunteerAndFamilyManager.UpdateVolunteerAndFamily(value);
+        }
+
+        // DELETE: api/VolunteerAndFamily/5
+        public void Delete(int id)
+        {
+            Bll.VolunteerAndFamilyManager.RemoveVolunteerAndFamily(id);
+        }
+    }
+}
