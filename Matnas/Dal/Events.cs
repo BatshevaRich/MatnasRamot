8ebece5fb258de8fb382fbb23fe7afe02904d1ab
Batsheva Rich <batsheva7424@gmail.com>
2020-02-27 14:01:27 +0200

    capitalized dateAdded

--- Matnas/Dal/Events.cs
+++ Matnas/Dal/Events.cs
@@ -23,7 +23,7 @@ namespace Dal
     
         public int Id { get; set; }
         public string description { get; set; }
-        public Nullable<System.DateTime> DateAdded { get; set; }
+        public Nullable<System.DateTime> dateAdded { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<VolunteerAndEvent> VolunteerAndEvent { get; set; }
