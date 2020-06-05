3c7713015044cd471a58bc9862b774c3c76bc4f1
Batsheva Rich <batsheva7424@gmail.com>
2020-06-05 17:14:21 +0300

    documented volunteer controller

--- Matnas/WebApi/WebApi/Controllers/VolunteerController.cs
+++ Matnas/WebApi/WebApi/Controllers/VolunteerController.cs
@@ -10,18 +10,32 @@ namespace WebApi.Controllers
 {
     public class VolunteerController : ApiController
     {
+        /// <summary>
+        /// Get all volunteers
+        /// </summary>
+        /// <returns>List of volunteers</returns>
         // GET: api/Volunteer
         public IHttpActionResult Get()
         {
             return Ok(Bll.VolunteerManager.GetVolunteers());
         }
 
+        /// <summary>
+        /// Get volunteer by id.
+        /// </summary>
+        /// <param name="id">Id of volunteer</param>
+        /// <returns>Volunteer object</returns>
         // GET: api/Volunteer/5
         public Volunteer Get(int id)
         {
             return Bll.VolunteerManager.GetVolunteer(id);
         }
 
+        /// <summary>
+        /// Post volunteer to db.
+        /// </summary>
+        /// <param name="data">Volunteer object and Category list</param>
+        /// <returns></returns>
         // POST: api/Volunteer
         public int Post([FromBody] JObject data)
         {
@@ -30,6 +44,10 @@ namespace WebApi.Controllers
             return Bll.VolunteerManager.AddVolunteer(newVolunteer, category);
         }
 
+        /// <summary>
+        /// Update volunteer by data sent.
+        /// </summary>
+        /// <param name="data">Volunteer object and Category list</param>
         [HttpPut]
         // PUT: api/Volunteer/5
         public void Put([FromBody] JObject data)
@@ -39,42 +57,74 @@ namespace WebApi.Controllers
             Bll.VolunteerManager.UpdateVolunteer(newVolunteer, category);
         }
 
+        /// <summary>
+        /// Delete volunteer from db.
+        /// </summary>
+        /// <param name="id">Id of volunteer</param>
         // DELETE: api/Volunteer/5
-        public HttpResponseMessage Delete(int id)
+        public void Delete(int id)
         {
-            try
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, new Exception("Error saving volunteer in database"));
-                // Bll.VolunteerManager.RemoveVolunteer(id);
-                
-            }
-            catch (Exception e)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, e);
-            }
-            
+            //return Request.CreateResponse(HttpStatusCode.BadRequest, new Exception("Error saving volunteer in database"));
+            Bll.VolunteerManager.RemoveVolunteer(id);
         }
-       
+
+        /// <summary>
+        /// Get categories of volunteers.
+        /// </summary>
+        /// <param name="id">Id of volunteer</param>
+        /// <returns>List of categories</returns>
         [Route("api/categoriesOfVolunteer")]
+        [HttpGet]
         public IEnumerable<Category> GetCategories(int id)
         {
             return Bll.VolunteerManager.GetCategories(id);
         }
+        
+        /// <summary>
+        /// Add category to volunteer
+        /// </summary>
+        /// <param name="id">Id of volunteer</param>
+        /// <param name="category">List of categories</param>
         [Route("api/addCategoryToVolunteer")]
         public void AddCategory(int id, [FromBody] Category[] category)
         {
             Bll.VolunteerManager.AddCategory(id, category);
         }
+        
+        /// <summary>
+        /// Remove category from volunteer
+        /// </summary>
+        /// <param name="id">Id of volunteer</param>
+        /// <param name="category">Category of volunteer</param>
         [Route("api/removeCategoryFromVolunteer")]
         public void RemoveCategory(int id, [FromBody] Category category)
-        {
+        {/////////////////need to check if works
             Bll.VolunteerManager.RemoveCategory(id, category);
         }
+        
+        /// <summary>
+        /// Get families of volunteer
+        /// </summary>
+        /// <param name="id">Id of volunteer</param>
+        /// <returns>List of families</returns>
         [Route("api/volunteer/families")]
+        [HttpGet]
         public IEnumerable<Family> GetFamilies(int id)
         {
             return Bll.VolunteerManager.GetFamilies(id);
         }
+
+        /// <summary>
+        /// Get volunteers by category sent.
+        /// </summary>
+        /// <param name="idCategory">Id of category</param>
+        /// <returns>List of volunteers</returns>
+        [Route("api/volunteer/volunteersbycategory/{idCategory}")]
+        public IEnumerable<Volunteer> GetVolunteersByCategory(int idCategory)
+        {
+            return Bll.VolunteerManager.GetVolunteersByCategory(idCategory);
+        }
+
         [Route("api/volunteer/groups")]
         public IEnumerable<Group> GetGroups(int id)
         {
@@ -84,17 +134,6 @@ namespace WebApi.Controllers
         public IEnumerable<Event> GetEvents(int id)
         {
             return Bll.VolunteerManager.GetEvents(id);
-        }
-        //[Route("api/volunteer/familyandcategory")]
-        //public IEnumerable<Volunteer> GetVolunteersByCategoryAndFamily(int idFamily, [FromBody] Category category)
-        //{
-        //    return Bll.VolunteerManager.GetVolunteersByCategoryAndFamily(idFamily, category);
-        //}
-
-        [Route("api/volunteer/volunteersbycategory/{idCategory}")]
-        public IEnumerable<Volunteer> GetVolunteersByCategory(int idCategory)
-        {
-            return Bll.VolunteerManager.GetVolunteersByCategory(idCategory);
         }
     }
 }
