1b37833eeb89719c4decff76b918b85d1575cd58
Batsheva Rich <batsheva7424@gmail.com>
2020-06-13 21:34:23 +0300

    add/update/remove funcs in organization service

--- rmatnas/src/app/services/organization.service.ts
+++ rmatnas/src/app/services/organization.service.ts
@@ -3,6 +3,7 @@ import { Organization } from '../Classes/Organization';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Category } from '../Classes/Category';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,31 @@ export class OrganizationService {
   constructor(private http: HttpClient) { }
   getOrganizations(): Observable<Organization[]> {
     return this.http.get<Organization[]>(this.path);
+  }
+  getOrganization(id: number): Observable<Organization> {
+    return this.http.get<Organization>(
+      this.path + '/' + id
+    );
+  }
+  addOrganization(myOrganization: Organization, categories: Category[]) {
+    // returns id to be updated in table
+    const myData = {} as any;
+    myData.Organization = myOrganization;
+    myData.categories = categories;
+    return this.http
+      .post(this.path, myData).toPromise().then(res => res);
+  }
+  updateOrganization(myOrganization: Organization, categoriesSelected: Category[]) {
+    // const head={params:new HttpParams() ('newOrganization',myOrganization)};
+    const myData = {} as any;
+    myData.Organization = myOrganization;
+    myData.categories = categoriesSelected;
+    this.http.put(
+      this.path, myData
+    ).subscribe();
+  }
+  removeOrganization(id: number) {
+    // const head={params:new HttpParams() ('newOrganization',myOrganization)};
+    this.http.delete(this.path + '/' + id).subscribe();
   }
 }
