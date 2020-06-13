8f11e9bd655f7a3fd1b2d7349eda0dbcaac0d3f3
Batsheva Rich <batsheva7424@gmail.com>
2020-06-13 21:52:52 +0300

    organization component

--- rmatnas/src/app/Components/single/organization/organization.component.ts
+++ rmatnas/src/app/Components/single/organization/organization.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { DataServiceService } from '../../../Services/data-service.service';
 import { ActivatedRoute } from '@angular/router';
 import { Organization } from 'src/app/Classes/Organization';
+import { Category } from 'src/app/Classes/Category';
+import { OrganizationService } from 'src/app/services/organization.service';
+import { MatDialog } from '@angular/material';
+import { OrganizationFComponent } from '../../forms/organization-f/organization-f.component';
 
 @Component({
   selector: 'app-organization',
@@ -11,25 +15,46 @@ import { Organization } from 'src/app/Classes/Organization';
 })
 export class OrganizationComponent implements OnInit, OnDestroy {
 
-  organization: Organization = new Organization(1, 'ברירת מחדל', 'ברירת מחדל', '11', 'ברירת מחדל', 'ברירת מחדל', '11@11');
+  chooseTab: string;
+  myOrganization: Organization;
+  categories: Category[] = [];
+  id: number;
+  @Input() vId: number;
+  @Input() toV: boolean;
+  @Output() addedOrganization: EventEmitter<Organization> = new EventEmitter<Organization>();
   mySubscription: Subscription;
-  Id: number;
-  constructor(public ds: DataServiceService, public ARS: ActivatedRoute) {
+  constructor(public vs: OrganizationService, public ARS: ActivatedRoute, public dialog: MatDialog) {
+    this.myOrganization = new Organization('...', '...', '...', '...', '...', '...');
+  }
 
-    this.mySubscription = ARS.params.subscribe((args) => {
-      this.Id = args.organizationId;
-      this.organization = ds.getOrganization(this.Id);
-      if (this.mySubscription) {
-      this.mySubscription.unsubscribe();
-    }
-    });
-  }
-  ngOnInit() {
-  }
   ngOnDestroy(): void {
     if (this.mySubscription) {
       this.mySubscription.unsubscribe();
     }
+
+  }
+
+  ngOnInit() {
+    this.vs.getOrganization(this.vId).subscribe(v => {
+      this.myOrganization = v;
+      // this.vs.getCategoriesOfOrganization(this.vId).subscribe(c => {
+      //   this.categories = c;
+      // });
+    });
+  }
+
+  OrganizationOpenDialog() {
+    const dialogRef = this.dialog.open(OrganizationFComponent, {
+      data: {
+        dataKey: this.myOrganization,
+        update: true,
+        id: this.myOrganization.Id,
+        chosenC: this.categories
+      }
+    });
+    dialogRef.afterClosed().subscribe(res => {
+      this.addedOrganization.emit(res);
+    });
   }
 
 }
