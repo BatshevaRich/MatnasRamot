00c4d201023eacfdf24c17eb56d8484705152216
Batsheva Rich <batsheva7424@gmail.com>
2020-06-14 02:35:11 +0300

    add organization form
    need to check if works with db

--- rmatnas/src/app/Components/forms/organization-f/organization-f.component.ts
+++ rmatnas/src/app/Components/forms/organization-f/organization-f.component.ts
@@ -1,6 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { Organization } from 'src/app/Classes/Organization';
 import { DataServiceService } from '../../../Services/data-service.service';
+import { NgForm, FormControl, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef, MatSnackBar } from '@angular/material';
+import { OrganizationService } from 'src/app/services/organization.service';
+import { Category } from 'src/app/Classes/Category';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-organization-f',
@@ -8,13 +13,66 @@ import { DataServiceService } from '../../../Services/data-service.service';
   styleUrls: ['./organization-f.component.css']
 })
 export class OrganizationFComponent implements OnInit {
+  @ViewChild('organizationForm') mytemplateForm: NgForm;
+  categories: Category[] = [];
+  categoriesOfOrganization: Category[] = [];
+  email = new FormControl('', [Validators.required, Validators.email]);
   newOrganization: Organization = new Organization('ברירת מחדל', 'ברירת מחדל', '11', 'ברירת מחדל', 'ברירת מחדל', '1@1');
-  constructor(public ds: DataServiceService) { }
+  token = 0;
+  categoriesSelected: Category[] = [];
+  constructor(public os: OrganizationService,
+              private cs: CategoryService,
+              private dialogRef: MatDialogRef<OrganizationFComponent>,
+              @Inject(MAT_DIALOG_DATA) public data: any,
+              public snackBar: MatSnackBar) {
+                cs.getCategories().subscribe(res => {
+                  this.categories = res;
+                });
+               }
 
   ngOnInit() {
+    if (this.data.update) {
+      this.newOrganization = this.data.dataKey;
+      this.categoriesOfOrganization = this.data.chosenC;
+    }
   }
+
+  selectCategories(e) {
+    this.categoriesSelected = [];
+    e.forEach(element => {
+      if (element.checked) {
+        this.categoriesSelected.push(new Category(element.name));
+      }
+    });
+  }
+
   submitForm(f) {
-    this.ds.addOrganization(new Organization(this.newOrganization.name, this.newOrganization.contact, this.newOrganization.phone, this.newOrganization.address, this.newOrganization.comments, this.newOrganization.email) );
-    f.reset();
+    if (this.data.update) {
+      this.newOrganization.Id = this.data.id;
+      this.os.updateOrganization(this.newOrganization, this.categoriesSelected);
+      this.dialogRef.close(this.newOrganization);
+    } else {
+      this.os.addOrganization(this.newOrganization, this.categoriesSelected)
+        .then(t => {
+          this.token = t as number;
+          /////////////////////// need to find out about safe casting in ts
+          this.newOrganization.Id = this.token;
+          this.categoriesSelected = [];
+          // this.addedVolunteer.emit(this.newVolunteer);
+          this.dialogRef.close(this.token);
+        });
+      this.mytemplateForm.resetForm();
+      this.newOrganization = new Organization('default', null, '000000000', null, null, 'default@ddd');
+      this.snackBar.open('שמירת ארגון מבוצעת...', 'OK', {
+        duration: 2000,
+        direction: 'rtl'
+      });
+    }
+  }
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'הערך שהוזן אינו תקני';
+    }
   }
 }
