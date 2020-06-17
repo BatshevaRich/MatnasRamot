d5e6357025ea7b0694a5e332cb4f4d184e4b7314
Batsheva Rich <batsheva7424@gmail.com>
2020-06-17 16:47:42 +0300

    cards to display which list needs attention, with buttons to route to component

--- rmatnas/src/app/Components/UI/home/home.component.ts
+++ rmatnas/src/app/Components/UI/home/home.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef, OnDestroy } from '@angular/core';
+import { MatDialog } from '@angular/material';
+import { NotificationService } from 'src/app/services/notification.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
-  constructor() { }
+  familyBadge = 0;
+  volunteerBadge = 0;
+  eventBadge = 0;
+  organizationBadge = 0;
+  categoryBadge = 0;
+  constructor(public dialog: MatDialog, public router: Router, public ns: NotificationService, private elementRef: ElementRef) {
+
+  }
+
+  ngAfterViewInit() {
+    this.familyBadge = this.ns.Families.length;
+    this.volunteerBadge = this.ns.Volunteers.length;
+    this.eventBadge = this.ns.Events.length;
+    this.organizationBadge = this.ns.Organizations.length;
+  }
 
   ngOnInit() {
+
+  }
+
+  ngOnDestroy(): void {
+    this.elementRef.nativeElement.remove();
   }
 
 }
