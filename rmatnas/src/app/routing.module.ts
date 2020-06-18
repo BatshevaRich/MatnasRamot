d9603a1a6ed60933fc93fb6844ae272711a4d278
Batsheva Rich <batsheva7424@gmail.com>
2020-06-18 14:10:58 +0300

    cleaned routes, left only used ones

--- rmatnas/src/app/routing.module.ts
+++ rmatnas/src/app/routing.module.ts
@@ -1,112 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './Components/UI/home/home.component';
-import { AddVFComponent } from './Components/forms/add/add-vf/add-vf.component';
 import { AllToVolunteersComponent } from './Components/all/all-to-volunteers/all-to-volunteers.component';
 import { AllFamiliesComponent } from './Components/all/all-families/all-families.component';
 import { AllOrganizationsComponent } from './Components/all/all-organizations/all-organizations.component';
 import { AllEventsComponent } from './Components/all/all-events/all-events.component';
 import { AllVolunteersComponent } from './Components/all/all-volunteers/all-volunteers.component';
-import { VolunteerComponent } from './Components/single/volunteer/volunteer.component';
-import { VolunteerFComponent } from './Components/forms/volunteer-f/volunteer-f.component';
-import { FamilyComponent } from './Components/single/family/family.component';
-import { FamilyFComponent } from './Components/forms/family-f/family-f.component';
-import { EventComponent } from './Components/single/event/event.component';
-import { EventFComponent } from './Components/forms/event-f/event-f.component';
 import { AllTasksComponent } from './Components/all/all-tasks/all-tasks.component';
 import { MyTaskComponent } from './Components/single/task/task.component';
-import { OrganizationComponent } from './Components/single/organization/organization.component';
-import { OrganizationFComponent } from './Components/forms/organization-f/organization-f.component';
 import { MainComponent } from './Components/UI/main/main.component';
+import { LoginComponent } from './Components/security/login/login.component';
 
 
 export const routeList: Routes = [
+  { path: 'login', component: LoginComponent },
   { path: '', component: HomeComponent },
-    { path: 'home', component: HomeComponent },
-    { path: 'main', component: MainComponent },
-    { path: 'addvf', component: AddVFComponent },
-
-    {
-      path: 'list',
-      children: [
-        {
-          path: 'vaf', component: AllToVolunteersComponent,
-          children: [
-            { path: ':vId/volunteers', component: AllFamiliesComponent },
-            { path: ':vId/families', component: AllOrganizationsComponent },
-            { path: ':vId/events', component: AllEventsComponent }
-          ]
-        },
-        {
-          path: 'volunteers', component: AllVolunteersComponent,
-          children: [
-            { path: ':vId/families', component: AllFamiliesComponent },
-            { path: ':vId/organizations', component: AllOrganizationsComponent },
-            { path: ':vId/events', component: AllEventsComponent }
-          ]
-        },
-        {
-          path: 'families', component: AllFamiliesComponent,
-          children: [
-            { path: ':vId/volunteers', component: AllVolunteersComponent },
-            { path: ':vId/organizations', component: AllOrganizationsComponent },
-            { path: ':vId/events', component: AllEventsComponent }
-          ]
-        },
-        {
-          path: 'volunteer/:volunteerId', component: VolunteerComponent,
-          children: [
-            { path: 'families', component: AllFamiliesComponent },
-            { path: 'organizations', component: AllOrganizationsComponent },
-            { path: 'events', component: AllEventsComponent }
-          ]
-        },
-        { path: 'add-volunteer', component: VolunteerFComponent },
-        { path: 'report-volunteer', component: VolunteerComponent },
-        { path: 'families', component: AllFamiliesComponent },
-        {
-          path: 'family/:familyId',
-          component: FamilyComponent,
-          children: [
-            { path: 'volunteers', component: AllVolunteersComponent },
-            { path: 'organizations', component: AllOrganizationsComponent },
-            { path: 'events', component: AllEventsComponent }
-          ]
-        },
-        { path: 'add-family', component: FamilyFComponent },
-        { path: 'report-family', component: FamilyComponent },
-        { path: 'events', component: AllEventsComponent },
-        {
-          path: 'event/:eventId',
-          component: EventComponent,
-          children: [
-            { path: 'families', component: AllFamiliesComponent },
-            { path: 'organizations', component: AllOrganizationsComponent },
-            { path: 'volunteers', component: AllVolunteersComponent }
-          ]
-        },
-        { path: 'add-event', component: EventFComponent },
-        { path: 'organizations', component: AllOrganizationsComponent },
-        { path: 'tasks', component: AllTasksComponent },
-        { path: 'task/:taskId', component: MyTaskComponent },
-        {
-          path: 'organization/:organizationId',
-          component: OrganizationComponent,
-          children: [
-            { path: 'families', component: AllFamiliesComponent },
-            { path: 'volunteers', component: AllVolunteersComponent },
-            { path: 'events', component: AllEventsComponent }
-          ]
-        },
-        { path: 'add-organization', component: OrganizationFComponent }
-      ]
-    }
-  ];
+  { path: 'home', component: HomeComponent },
+  { path: 'main', component: MainComponent },
+  {
+    path: 'list', children: [
+      { path: 'vaf', component: AllToVolunteersComponent },
+      { path: 'volunteers', component: AllVolunteersComponent },
+      { path: 'families', component: AllFamiliesComponent },
+      { path: 'events', component: AllEventsComponent },
+      { path: 'organizations', component: AllOrganizationsComponent },
+      { path: 'tasks', component: AllTasksComponent },
+      { path: 'task/:taskId', component: MyTaskComponent }
+    ]
+  }
+];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routeList) ],
+  imports: [RouterModule.forRoot(routeList)],
   exports: [
-      RouterModule
-   ]
+    RouterModule
+  ]
 })
 export class RoutingModule { }
