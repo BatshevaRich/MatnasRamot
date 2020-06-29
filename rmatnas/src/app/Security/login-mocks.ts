582b5693e42c75726b47de4b6e1488c472860d38
Batsheva Rich <batsheva7424@gmail.com>
2020-06-29 23:56:34 +0300

    trying login, needs work

--- rmatnas/src/app/Security/login-mocks.ts
+++ rmatnas/src/app/Security/login-mocks.ts
@@ -1,3 +1,14 @@
 import { UserAuth } from '../Classes/UserAuth';
 
-export const LOGIN_MOCKS: UserAuth[] = [];
+export const LOGIN_MOCKS: UserAuth[] = [
+    {
+        userName: 'BJones',
+        bearerToken: 'sd9f923k3kdmcjkhd',
+        isAuthenticated: true,
+        canAccessVolunteers: true,
+        canAddVolunteer: false,
+        canSaveVolunteer: false,
+        canAccessCategories: true,
+        canAddCategory: true
+      }
+];
