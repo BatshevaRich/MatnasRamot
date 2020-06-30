b49e4058961050c7e59008493596dd44ea0710c7
Batsheva Rich <batsheva7424@gmail.com>
2020-07-01 00:21:51 +0300

    Revert "Merge branch 'login'"
    This reverts commit acbb7d464e3f287fd481f8d00702fa39a7c7978f, reversing
    changes made to 8eb04166ff011ee13aafa0be2ce89026cb467044.

--- rmatnas/src/app/Security/login-mocks.ts
+++ rmatnas/src/app/Security/login-mocks.ts
@@ -1,14 +1,3 @@
 import { UserAuth } from '../Classes/UserAuth';
 
-export const LOGIN_MOCKS: UserAuth[] = [
-    {
-        userName: 'BJones',
-        bearerToken: 'sd9f923k3kdmcjkhd',
-        isAuthenticated: true,
-        canAccessVolunteers: true,
-        canAddVolunteer: false,
-        canSaveVolunteer: false,
-        canAccessCategories: true,
-        canAddCategory: true
-      }
-];
+export const LOGIN_MOCKS: UserAuth[] = [];
--- rmatnas/src/app/app.component.ts
+++ rmatnas/src/app/app.component.ts
@@ -5,8 +5,6 @@ import { VolunteerFComponent } from './Components/forms/volunteer-f/volunteer-f.
 import { FamilyFComponent } from './Components/forms/family-f/family-f.component';
 import { AllVolunteersComponent } from './Components/all/all-volunteers/all-volunteers.component';
 import { AddVFComponent } from './Components/forms/add/add-vf/add-vf.component';
-import { UserAuth } from './Classes/UserAuth';
-import { SecurityService } from './Security/security.service';
 
 @Component({
   selector: 'app-root',
@@ -14,15 +12,9 @@ import { SecurityService } from './Security/security.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  securityObject: UserAuth = null;
   title = 'endproject';
-  constructor(private securityService: SecurityService) {
-    this.securityObject = securityService.securityObject;
+  constructor() {
 
-  }
-
-  logout(): void {
-    this.securityService.logout();
   }
 
 }
