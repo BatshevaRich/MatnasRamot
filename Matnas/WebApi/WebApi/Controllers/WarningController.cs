b0d4479d9895d3119fcfa037df2f2374b5448a3d
Batsheva Rich <batsheva7424@gmail.com>
2020-07-07 10:59:10 +0300

    Create WarningController.cs

---
+++ Matnas/WebApi/WebApi/Controllers/WarningController.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class WarningController : ApiController
+    {
+        // GET: api/Warning
+        [Route("api/warning/family")]
+        public IEnumerable<Family> Get()
+        {
+            return Bll.FamilyManager.GetFamiluesWithoutVolunteer();
+        }
+
+        // GET: api/Warning
+        [Route("api/warning/event")]
+        public IEnumerable<Event> GetEvents()
+        {
+            return Bll.EventManager.GetNextEvents();
+        }
+    }
+}
