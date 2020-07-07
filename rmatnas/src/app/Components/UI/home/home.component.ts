028af5689cf69ef1e6578ef307c05b2304078a52
Batsheva Rich <batsheva7424@gmail.com>
2020-07-07 13:05:26 +0300

    show notification only when exists

--- rmatnas/src/app/Components/UI/home/home.component.ts
+++ rmatnas/src/app/Components/UI/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, AfterViewInit, ElementRef, OnDestroy } from '@angula
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationService } from '../../../services/notification.service';
 import { Router } from '@angular/router';
+import { Eventt } from '../../../Classes/Eventt';
+import { Family } from '../../../Classes/Family';
 
 @Component({
   selector: 'app-home',
@@ -15,15 +17,23 @@ export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
   eventBadge = 0;
   organizationBadge = 0;
   categoryBadge = 0;
-  constructor(public dialog: MatDialog, public router: Router, public ns: NotificationService, private elementRef: ElementRef) {
-
+  constructor(public dialog: MatDialog,
+              public router: Router,
+              public ns: NotificationService,
+              private elementRef: ElementRef) {
+    this.ns.getAllFamiliesToConnect().subscribe((res: Family[]) => {
+      this.familyBadge = res.length;
+    });
+    this.ns.getAllEventsToConnect().subscribe((res: Eventt[]) => {
+      this.eventBadge = res.length;
+    });
   }
 
   ngAfterViewInit() {
-    this.familyBadge = this.ns.Families.length;
-    this.volunteerBadge = this.ns.Volunteers.length;
-    this.eventBadge = this.ns.Events.length;
-    this.organizationBadge = this.ns.Organizations.length;
+    // this.familyBadge = this.ns.Families.length;
+    // this.volunteerBadge = this.ns.Volunteers.length;
+    // this.eventBadge = this.ns.Events.length;
+    // this.organizationBadge = this.ns.Organizations.length;
   }
 
   ngOnInit() {
