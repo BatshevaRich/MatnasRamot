f7a7b518da1896c49debc7e41dbe7ff6d4c15a23
Batsheva Rich <batsheva7424@gmail.com>
2020-07-10 16:16:47 +0300

    show changes in all events table

--- rmatnas/src/app/Components/all/all-events/all-events.component.ts
+++ rmatnas/src/app/Components/all/all-events/all-events.component.ts
@@ -135,7 +135,32 @@ export class AllEventsComponent implements OnInit, OnDestroy, AfterViewInit {
     const dialogRef = this.dialog.open(AddVEComponent, {
       maxWidth: '75%',
       data: {
-        id: event.Id
+        id: event.Id,
+        type: 'event',
+        eventt: event
+      }
+    });
+    dialogRef.afterClosed().subscribe(id => {
+      if (id) {
+        const removeIndex = this.ns.Events.map((item) => item.Id).indexOf(id);
+        this.dataSource.data[removeIndex].color = false;
+        // tslint:disable-next-line: no-bitwise
+        const removed = ~removeIndex && this.ns.Events.splice(removeIndex, 1);
+        const res = this.ns.Events;
+        function sortFunc(a: { Id: number; }, b: { Id: number; }) {
+          const s1 = res.find(s => s.Id === a.Id);
+          const s2 = res.find(s => s.Id === b.Id);
+          if (s1 && s2) { return 0; }
+          else if (s1) { return -1; }
+          else if (s2) { return 1; }
+          return 0;
+        }
+        const sorted = this.dataSource.data.sort(sortFunc);
+        for (let index = 0; index < res.length; index++) {
+          sorted[index].color = true;
+        }
+        this.dataSource.data = sorted;
+        this.ns.Events = sorted;
       }
     });
     return dialogRef.afterClosed();
