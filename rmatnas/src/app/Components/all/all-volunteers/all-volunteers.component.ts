f1ff5a9e80e4b861aee04767d9288c153d271df3
Batsheva Rich <batsheva7424@gmail.com>
2020-07-11 22:30:39 +0300

    Update all-volunteers.component.ts

--- rmatnas/src/app/Components/all/all-volunteers/all-volunteers.component.ts
+++ rmatnas/src/app/Components/all/all-volunteers/all-volunteers.component.ts
@@ -164,8 +164,8 @@ export class AllVolunteersComponent implements OnInit, OnDestroy, AfterViewInit 
       this.result = res;
       if (res) {
         this.vs.removeVolunteer(elm.Id);
-        this.dataSource = new MatTableDataSource(Object.values(this.dataSource)
-          .filter(i => i !== elm));
+        this.dataSource.data = this.dataSource.data
+          .filter(i => i !== elm);
         // .map((i, idx) => (i.position = (idx + 1), i));
       }
     });
