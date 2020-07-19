2a491b4f0487e1c6a9d9e7dee6ce8ec519785115
Batsheva Rich <batsheva7424@gmail.com>
2020-07-19 23:36:12 +0300

    display notifs of most used

--- rmatnas/src/app/Components/UI/home/home.component.ts
+++ rmatnas/src/app/Components/UI/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, AfterViewInit, ElementRef, OnDestroy, ChangeDetector
 import { MatDialog } from '@angular/material/dialog';
 import { NotificationService } from '../../../services/notification.service';
 import { Router } from '@angular/router';
+import { Volunteer } from '../../../Classes/Volunteer';
+import { Family } from '../../../Classes/Family';
 
 @Component({
   selector: 'app-home',
@@ -9,14 +11,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
-
+  panelOpenState = false;
   familyBadge = 0;
   eventBadge = 0;
+  closestEvent: string;
+  DateClosestEvent: string;
+  DaysToEvent: number;
+  mostVolunteer: Volunteer;
+  mostFamily: Family;
   constructor(public dialog: MatDialog,
               public router: Router,
               public ns: NotificationService,
               private elementRef: ElementRef,
-              private cdr: ChangeDetectorRef) { }
+              private cdr: ChangeDetectorRef) {
+    const temp = this.ns.Events.map(d => Math.abs(Date.now() - new Date(d.StartDate).getTime()));
+    const idx = temp.indexOf(Math.min(...temp));
+    this.closestEvent = this.ns.Events[idx].Name;
+    this.DateClosestEvent = this.ns.Events[idx].StartDate;
+    const d = this.ns.Events[idx].StartDate.slice(0, 10).split('-');
+    const x = d[1] + '/' + d[2] + '/' + d[0];
+    const diff = Math.abs(new Date(x).getTime() - new Date().getTime());
+    this.DaysToEvent  = Math.ceil(diff / (1000 * 3600 * 24)); 
+    this.ns.getMostVolunteer().subscribe((res: Volunteer) => {
+      this.mostVolunteer = res;
+    });
+    this.ns.getMostFamily().subscribe((res: Family) => {
+      this.mostFamily = res;
+    });
+  }
 
   ngAfterViewInit() {
     this.familyBadge = this.ns.Families.length;
